08b65dcd28ebf7c6c3e44d7e8e1919de1cc50e79
DataReports <data.reports@greentomatocars.com>
2017-06-19 10:49:35 +0100

    added close and open outlook code to batch

--- SalesPerson_Weekly.bat
+++ SalesPerson_Weekly.bat
@@ -1,2 +1,5 @@
-@echo on
+taskkill /IM Outlook.exe /f
+start "" "%ProgramFiles(x86)%\Microsoft Office\Office14\outlook.exe"
+
+@echo on@echo on
 "C:\Program Files\R\R-3.2.3\bin\R.exe" CMD BATCH C:\Programs\gtc_tasks\SalesPerson_Weekly\SalesPerson_Weekly.R
